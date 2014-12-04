427f89f214e2a395a18391b213d7a17e50e2a85d
beck410 <beck410@hotmail.com>
2014-12-04 14:30:34 -0600

    example of angular controllers

--- js/main.js
+++ js/main.js
@@ -2,4 +2,13 @@
   'use strict';
 
   var hello = 'world';  
+
+  //relates to dependency injection
+  angular.module('myApp', [])
+    .controller('SimpleController',function(){
+      var vm= this;
+      vm.name = "beck";
+      vm.friends = ['Dave','bob','penny','jezzy']
+  });
 }());
+
