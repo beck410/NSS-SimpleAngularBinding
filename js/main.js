8424799516954624e05273e6b60387a9af0a06b6
beck410 <beck410@hotmail.com>
2014-12-04 15:06:22 -0600

    new tasks controller added

--- js/main.js
+++ js/main.js
@@ -7,6 +7,27 @@
   angular.module('myApp', [])
     .controller('TodoController',function(){
       var vm= this;
+      vm.tasks = [
+        {
+          name: 'Learn angular',
+          desc: 'If i could learn Angular id be so happy',
+          due: 'Today'
+        },
+        {
+          name: 'Finish Tic Tac Toe',
+          desc: 'firebase is hell',
+          due: 'Monday'
+        },
+        {
+          name: 'Get a job',
+          desc: 'yay money!',
+          due: 'April 2015'
+        }
+      ];
+
+      vm.addNewTasks = function(){
+        vm.tasks.push(vm.newTask);
+      }
 
   });
 }());
